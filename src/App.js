2aa37c2c5aca615c6a79961942052f7ceb793d9e
queirozdaniel <danielqueirozak77@gmail.com>
2022-03-09 22:25:30 -0300

    add: assets

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+import './App.css';
+import Home from './Components/Home';
+import Login from './Components/Login/Login';
+
 function App() {
   return (
     <div>
-      <header></header>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
