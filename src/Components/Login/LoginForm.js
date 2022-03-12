b0dca2a349573ce11fb06d3e71f262b5523449b7
queirozdaniel <danielqueirozak77@gmail.com>
2022-03-11 23:38:19 -0300

    feat: Using external function to provide options to fetch data

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { TOKEN_POST } from '../../API';
 import useForm from '../../Hooks/useForm';
 import Button from '../Forms/Button';
 import Input from '../Forms/Input';
@@ -12,13 +13,11 @@ const LoginForm = () => {
     event.preventDefault();
 
     if (username.validate() && password.validate()) {
-      fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ username, password }),
-      })
+      const { url, options } = TOKEN_POST({
+        username: username.value,
+        password: password.value,
+      });
+      fetch(url, options)
         .then((response) => {
           console.log(response);
           return response.json();
