16b84c6770d6f320fb096dd881597e7822e3b871
queirozdaniel <danielqueirozak77@gmail.com>
2022-03-15 21:57:12 -0300

    refactor: Change title to name of user

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -15,7 +15,7 @@ const Header = () => {
         </Link>
         {data ? (
           <Link className={styles.login} to="/conta">
-            Minha Conta
+            {data.nome}
           </Link>
         ) : (
           <Link className={styles.login} to="/login">
