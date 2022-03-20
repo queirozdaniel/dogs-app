f70474149c2a6181317f9b18fc8de50907aae35a
queirozdaniel <danielqueirozak77@gmail.com>
2022-03-19 23:47:42 -0300

    feat: Comments of PhotoPosts

--- src/Components/Photo/PhotoCommentForm.js
+++ src/Components/Photo/PhotoCommentForm.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import { COMMENT_POST } from '../../api';
 import { ReactComponent as Enviar } from '../../Assets/enviar.svg';
+import useFetch from '../../Hooks/useFetch';
 
 const PhotoCommentForm = ({ id }) => {
   const [comment, setComment] = React.useState('');
+  const { request, error } = useFetch();
 
-  console.log('rwas');
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const { url, options } = COMMENT_POST(id, { comment });
+    request(url, options);
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <textarea
+        id="comment"
+        name="comment"
+        placeholder="Comente..."
         value={comment}
         onChange={({ target }) => setComment(target.value)}
       />
--- src/Components/Photo/PhotoComments.js
+++ src/Components/Photo/PhotoComments.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import { UserContext } from '../../UserContext';
 import PhotoCommentForm from './PhotoCommentForm';
+import styles from './PhotoComments.module.css';
 
-const PhotoComments = ({ id, comments }) => {
+const PhotoComments = (props) => {
   const { login } = React.useContext(UserContext);
-  return <div>{login && <PhotoCommentForm id={id} />}</div>;
+  const [comments, setComments] = React.useState(() => props.comments);
+  return (
+    <>
+      <ul style={styles.comment}>
+        {comments.map((comment) => (
+          <li key={comment.comment_ID}>
+            <b>{comment.comment_author}</b>
+            <span>{comment.comment_content}</span>
+          </li>
+        ))}
+      </ul>
+      {login && <PhotoCommentForm id={props.id} />}
+    </>
+  );
 };
 
 export default PhotoComments;
