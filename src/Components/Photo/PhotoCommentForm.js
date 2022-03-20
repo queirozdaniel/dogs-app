426e55392b349123e9f1a0fe133c033c3745d1b7
queirozdaniel <danielqueirozak77@gmail.com>
2022-03-20 19:43:37 -0300

    refactor: Modify Photo Components

--- src/Components/Photo/PhotoCommentForm.js
+++ src/Components/Photo/PhotoCommentForm.js
@@ -2,29 +2,38 @@ import React from 'react';
 import { COMMENT_POST } from '../../api';
 import { ReactComponent as Enviar } from '../../Assets/enviar.svg';
 import useFetch from '../../Hooks/useFetch';
+import Error from '../Helper/Error';
+import styles from './PhotoCommentForm.module.css';
 
-const PhotoCommentForm = ({ id }) => {
+const PhotoCommentForm = ({ id, setComments }) => {
   const [comment, setComment] = React.useState('');
   const { request, error } = useFetch();
 
   async function handleSubmit(event) {
     event.preventDefault();
     const { url, options } = COMMENT_POST(id, { comment });
-    request(url, options);
+    const { response, json } = await request(url, options);
+
+    if (response.ok) {
+      setComment('');
+      setComments((comments) => [...comments, json]);
+    }
   }
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form className={styles.form} onSubmit={handleSubmit}>
       <textarea
+        className={styles.textarea}
         id="comment"
         name="comment"
         placeholder="Comente..."
         value={comment}
         onChange={({ target }) => setComment(target.value)}
       />
-      <button>
+      <button className={styles.button}>
         <Enviar />
       </button>
+      <Error error={error} />
     </form>
   );
 };
--- src/Components/Photo/PhotoComments.js
+++ src/Components/Photo/PhotoComments.js
@@ -5,18 +5,24 @@ import styles from './PhotoComments.module.css';
 
 const PhotoComments = (props) => {
   const { login } = React.useContext(UserContext);
+  const commentsSection = React.useRef(null);
   const [comments, setComments] = React.useState(() => props.comments);
+
+  React.useEffect(() => {
+    commentsSection.current.scrollTop = commentsSection.current.scrollHeight;
+  }, [comments]);
+
   return (
     <>
-      <ul style={styles.comment}>
+      <ul ref={commentsSection} className={styles.comments}>
         {comments.map((comment) => (
           <li key={comment.comment_ID}>
-            <b>{comment.comment_author}</b>
+            <b>{comment.comment_author}: </b>
             <span>{comment.comment_content}</span>
           </li>
         ))}
       </ul>
-      {login && <PhotoCommentForm id={props.id} />}
+      {login && <PhotoCommentForm id={props.id} setComments={setComments} />}
     </>
   );
 };
