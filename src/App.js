6588415b64512522297b9d4a0806bf7127a6b8c7
queirozdaniel <danielqueirozak77@gmail.com>
2022-03-23 22:01:36 -0300

    feat: Page of user Posts

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import User from './Components/User/User';
 import { UserStorage } from './UserContext';
 import ProtectedRoute from './Components/Helper/ProtectedRoute';
 import Photo from './Components/Photo/Photo';
+import UserProfile from './Components/User/UserProfile';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="login/*" element={<Login />} />
             <Route path="foto/:id" element={<Photo />} />
+            <Route path="perfil/:user" element={<UserProfile />} />
             <Route
               path="conta/*"
               element={
